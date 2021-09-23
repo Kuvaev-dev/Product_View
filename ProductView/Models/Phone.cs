20197cbcd5237568078c1c61e9d441576520a875
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-09-23 22:01:54 +0300

    Update Phone.cs

--- ProductView/Models/Phone.cs
+++ ProductView/Models/Phone.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Products_ASP.Models
+namespace Products_View.Models
 {
     public class Phone
     {
