9ad02bbefb24c67b556e25b56904736cbd12ff59
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-09-26 00:32:19 +0300

    Update Index.cshtml.cs

--- ProductView/Models/Content/Index.cshtml.cs
+++ ProductView/Models/Content/Index.cshtml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Products_ASP.Models;
+using ProductView.Models;
 
 namespace ProductView.Views.Home
 {
